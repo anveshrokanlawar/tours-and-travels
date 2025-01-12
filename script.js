8b81c943e8cf0a01b51b95757fd6cea71231532b
Anvesh Rao <anveshrao1616@gmail.com>
2025-01-12 15:20:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -59,4 +59,65 @@ function scrollPackages(direction) {
         const modal = document.getElementById(modalId);
         modal.style.display = 'none';
     }
+	
+		        document.querySelectorAll('.grid-item').forEach(item => {
+            let images = item.getAttribute('data-images').split(',');
+            let index = 0;
+            let interval;
+
+            item.addEventListener('mouseover', () => {
+                interval = setInterval(() => {
+                    index = (index + 1) % images.length;
+                    item.style.backgroundImage = `url(${images[index]})`;
+                }, 500);
+            });
+
+            item.addEventListener('mouseout', () => {
+                clearInterval(interval);
+                item.style.backgroundImage = `url(${images[0]})`;
+            });
+        });
+
+        document.querySelectorAll('.book-now').forEach(button => {
+            button.addEventListener('click', () => {
+                document.getElementById('booking-modal').style.display = 'flex';
+            });
+        });
+
+        document.querySelector('.close-modal').addEventListener('click', () => {
+            document.getElementById('booking-modal').style.display = 'none';
+        });
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('.modal-content form');  // Use the correct form selector
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();  // Prevent the default form submission
+
+            emailjs.sendForm('xmd5zzc', '014k9xn', form)  // Replace with actual service ID and template ID
+                .then(function(response) {
+                    showThankYouModal();  // Show the thank you modal on successful submission
+                    form.reset();
+                }, function(error) {
+                    showErrorModal();  // Show error modal if something goes wrong
+                    console.log(error);
+                });
+        });
+    });
+	
+	    // Function to set the selected grid value
+    function setGridValue(gridName) {
+        document.getElementById('selectedGrid').value = gridName;
+    }
+
+    // Attach the click event to each grid item
+    document.querySelectorAll('.grid-item').forEach(item => {
+        item.addEventListener('click', function() {
+            const gridName = item.querySelector('h2').innerText; // Get the grid name (e.g., Telangana, Gujarat)
+            setGridValue(gridName); // Set the value of the hidden field
+        });
+    });
+		
+		
 
